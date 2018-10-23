7dc2921407541f3023cda2a1e27e8aa548517b5b
hyegibang <hyegi.bang@students.olin.edu>
2018-10-23 09:17:52 -0400

    Clean Arduino Code

--- Python-Arduino/Python-Arduino.ino
+++ Python-Arduino/Python-Arduino.ino
@@ -12,28 +12,26 @@ void loop() {
   if(Serial.available() > 0){
       serialData = Serial.read();
       serialInt = serialData;
-      Serial.println(serialData);
-      Serial.print(serialInt);
-    
-      if(serialInt == 0){
-        digitalWrite(pin,LOW);
-        digitalWrite(pint,HIGH);
-        }
-        
-      else if(serialInt == 1){
-        digitalWrite(pin,HIGH);
-        digitalWrite(pint,LOW);
-        }
-        
-      else if(serialInt == 10){
-        digitalWrite(pin,HIGH);
-        digitalWrite(pint,LOW);
-        }
 
-      else if(serialInt == 11){
-        digitalWrite(pin,LOW);
+      switch(serialInt){
+        case 0:
+          digitalWrite(pin,LOW);
+          digitalWrite(pint,HIGH);
+          break;
+        case 1:
+          digitalWrite(pin,HIGH);
+          digitalWrite(pint,LOW);
+          break;
+        case 10:
+          digitalWrite(pin,HIGH);
+          digitalWrite(pint,LOW);
+          break;
+        case 11:
+          digitalWrite(pin,LOW);
         digitalWrite(pint,HIGH);
-        }
+       }
+     
+      
   }
   }
   
