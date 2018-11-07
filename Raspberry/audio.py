613f64afeeab50aacd65f71e490ec7fb6891aa92
hyegibang <hyegi.bang@students.olin.edu>
2018-11-07 14:51:42 -0500

    !

--- Raspberry/audio.py
+++ Raspberry/audio.py
@@ -1,15 +1,25 @@
-import pyttsx3
+import math
+import time
 
-engine = pyttsx3.init()
-rate = engine.getProperty('rate')
-volume =engine.getProperty('volume')
-voices = engine.getProperty('voices')
+import Adafruit_CharLCD as LCD
 
-engine.setProperty('rate', rate-30)
-engine.setProperty('volume', volume + 10)
-engine.setProperty('voice', 'english+f1') # or voices[13].id
+# Initialize the LCD using the pins
+lcd = LCD.Adafruit_CharLCDPlate()
 
+# Make list of button value, text, and backlight color.
+buttons = ( (LCD.SELECT,
+            (LCD.LEFT,
+            (LCD.UP,
+            (LCD.DOWN,
+            (LCD.RIGHT,)
 
-engine.say('Mary Had A Little Lamb')
-engine.runAndWait()
-
+# print 'Press Ctrl-C to quit.'
+# while True:
+# 	# Loop through each button and check if it is pressed.
+# 	for button in buttons:
+# 		if lcd.is_pressed(button[0]):
+# 			# Button is pressed, change the message and backlight.
+# 			lcd.clear()
+# 			lcd.message(button[1])
+# 			lcd.set_color(button[2][0], button[2][1], button[2][2])
+#
